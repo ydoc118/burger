270c0685f5abd2cd60e61e6fe8ddfe90f403008f
Cody <ydoc118@gmail.com>
2020-08-21 14:47:49 -0400

    first commit created all folders/files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Krabby Patty", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Ponch Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Good Burger", true);
